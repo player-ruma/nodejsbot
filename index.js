c5188f7b4fb91cb2d657648c6681dcca01970ef5
minecraftuserpalyer <saug-saugunil@naver.com>
2020-08-03 22:11:24 +0900

    ok

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ client.on('message', (message) => {
   if(message.content === '출석') {
     message.reply('출석완료');
   }
+
+if(message.content === '안녕') {
+  message.reply('안녕하세요');
+}
+
+if(message.content === 'ANNA살리라') {
+  message.reply('출석도 꾸준히! 대화도 많이! ANNA클 우리가 살리자!');
+}
+
+if(message.content === 'ping') {
+  message.reply('pong');
+}
 });
 
 client.login(token);
